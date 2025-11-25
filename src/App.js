9ffe04d8631e92c87753154e1d47c8184b0ad1aa
kurataniasami <kura56135613@gmail.com>
2025-11-25 20:56:02 +0900

    App.jsを変更

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+import PostDetail from './PostDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App({ posts }) {
+  const [upDatePosts, setUpdatePosts] = useState(window.location.hash);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleHashChange = () => setUpdatePosts(window.location.hash);
+    window.addEventListener('hashchange', handleHashChange);
+    setLoading(false);
+  }, []);
+
+  if (upDatePosts.startsWith('#/post/')) {
+    const id = upDatePosts.replace('#/post/', '');
+    return <PostDetail id={id} posts={posts} />;
+  }
+
+  return <BlogList posts={posts} />;
 }
-
-export default App;
